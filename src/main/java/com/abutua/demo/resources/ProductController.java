bcd84d690cbae70cc01642c001bef420316b0843
MateusL21 <mateusleonel27@gmail.com>
2024-08-21 19:52:02 -0300

    First commit

---
+++ src/main/java/com/abutua/demo/resources/ProductController.java
@@ -0,0 +1,59 @@
+package com.abutua.demo.resources;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.abutua.demo.models.Product;
+
+import jakarta.annotation.PostConstruct;
+
+@RestController
+@CrossOrigin
+public class ProductController {
+
+    private List<Product> products = new ArrayList<>();
+    
+    @PostMapping("products")
+    public ResponseEntity<Product> save(@RequestBody Product product) {
+        product.setId(products.size() + 1);
+        products.add(product);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(product.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(product);
+    }
+
+    @GetMapping("products/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+
+        Product prod = products.stream()
+                .filter(p -> p.getId() == id).findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "product not found"));
+
+        return ResponseEntity.ok(prod);
+    }
+
+    @GetMapping("products")
+    public List<Product> GetProducts() {
+        return products;
+
+    }
+
+}
